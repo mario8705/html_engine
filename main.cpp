dff006535692ea4f0e5cff74d2cfecfdf5ba9e89
Alexis Lavaud <alexislavaud191@gmail.com>
2018-03-17 19:25:26 +0100

    Basic dom construction

--- main.cpp
+++ main.cpp
@@ -3,240 +3,115 @@
 #include <vector>
 #include <string>
 #include <cstring>
+#include "parser.h"
 
-// 1 Mb
-#define MAX_FILE_BUFFER (1 * 1024 * 1024)
-
-struct buf_it
+class HTMLElement
 {
-    char *start;
-    char *end;
+public:
+    HTMLElement(const std::string &tagName, HTMLElement *parent = nullptr)
+        : m_tagName(tagName), m_parent(parent)
+    {
+    }
+
+    std::string GetTagName() const
+    {
+        return m_tagName;
+    }
+
+    void AddChildren(HTMLElement *element)
+    {
+        m_childrens.push_back(element);
+    }
+
+    bool IsRootElement() const
+    {
+        return m_parent == nullptr;
+    }
+
+    std::string GetData() const
+    {
+        return m_data;
+    }
+
+    void SetData(std::string data)
+    {
+        m_data = data;
+    }
+
+    void PrintTree(int depth = 1)
+    {
+        if (depth == 1) printf("%s\n", m_tagName.c_str());
+
+        for (HTMLElement *child : m_childrens)
+        {
+            int ident = depth;
+            while (ident--)
+            {
+                printf("  ");
+            }
+
+            printf("%s (%s)\n", child->GetTagName().c_str(), child->GetData().c_str());
+            child->PrintTree(depth + 1);            
+        }
+    }
+
+private:
+    HTMLElement *m_parent;
+    std::vector<HTMLElement *> m_childrens;
+    std::string m_tagName;
+    std::string m_data;
 };
 
-static char *load_all(const char *path, size_t *out_sz)
+class HTMLDocument
 {
-    FILE* fp;
-    char *buf;
-    unsigned long long sz;
-
-    if ((fp = fopen(path, "rb")) == NULL)
+public:
+    HTMLDocument()
     {
-        return NULL;
     }
-
-    fseek(fp, 0, SEEK_END);
-    sz = ftell(fp);
-    fseek(fp, 0, SEEK_SET);
-
-    if (sz >= MAX_FILE_BUFFER)
-    {
-        fprintf(stderr, "File size exceeds max buffer size\n");
-        fclose(fp);
-        return NULL;
-    }
-
-    buf = (char *)malloc(sz);
-    if (!buf)
-    {
-        fprintf(stderr, "Could not alloc the required memory\n");
-        fclose(fp);
-        return NULL;
-    }
-
-    if (fread(buf, 1, sz, fp) != sz)
-    {
-        fprintf(stderr, "Could not read the file\n");
-        free(buf);
-        fclose(fp);
-        return NULL;
-    }
-
-    *out_sz = sz;
-
-    fclose(fp);
-    return buf;
-}
-
-static bool char_is_any(char ch, const char *any)
-{
-    while (*any)
-    {
-        if (ch == *any)
-        {
-            return true;
-        }
-
-        ++any;
-    }
-
-    return false;
-}
-
-static char *parse_ident(buf_it *it, char* buf)
-{
-    char *pbuf = buf, ch;
-
-    while (it->start != it->end)
-    {
-        ch = *it->start;
-
-        if (isalpha(ch) || (pbuf != buf && (isalnum(ch) || char_is_any(ch, "-"))))
-        {
-            *buf++ = ch;
-            ++it->start;
-        }
-        else
-        {
-            break;
-        }
-    }
-
-    if (buf == pbuf)
-    {
-        return NULL;
-    }
-
-    *buf = '\0';
-    return pbuf;
-}
-
-static void trim_buf(buf_it *it)
-{
-    while (it->start != it->end && isspace(*it->start))
-    {
-        ++it->start;
-    }
-}
-
-// Checks if the buffer contains "<!--" at the current position
-static bool is_open_comment(buf_it *it)
-{
-    if (it->end - it->start >= 4)
-    {
-        if (strcmp(it->start, "<!--") == 0)
-        {
-            return true;
-        }
-    }
-
-    return false;
-}
-
-static bool is_close_comment(buf_it *it)
-{
-    if (it->end - it->start >= 3)
-    {
-        if (strcmp(it->start, "-->") == 0)
-        {
-            return true;
-        }
-    }
-
-    return false;
-}
-
-#define HT_OpenTag 1
-#define HT_CloseTag 2
-#define HT_Data 3
-
-struct HTMLToken
-{
-    int type;
-    char data[128];
-
-    HTMLToken(int type)
-        : type(type)
+    
+    ~HTMLDocument()
     {
     }
 };
 
-static void parse(buf_it *it, std::vector<HTMLToken> &tokens)
+using HTMLTokenIterator = std::vector<HTMLToken>::const_iterator;
+
+static void assemble_dom(HTMLElement *parent, HTMLTokenIterator start, HTMLTokenIterator end)
 {
-    char ident_buf[64];
-    bool close_tag;
-    char data_buf[128];
-    char *pdata = data_buf;
+    HTMLElement *element = nullptr;
 
-    while (it->start != it->end)
+    while (start != end)
     {
-        if (*it->start == '<')
+        const HTMLToken &tok = *start;
+
+        if (tok.type == HT_OpenTag)
         {
-            ++it->start;
-
-            trim_buf(it);
-
-            if (*it->start == '/')
-            {
-                close_tag = true;
-                ++it->start;
-                trim_buf(it);
-            }
-            else
-            {
-                close_tag = false;
-            }
-
-            HTMLToken tok(close_tag ? HT_CloseTag : HT_OpenTag);
-            parse_ident(it, tok.data);
-
-            tokens.push_back(tok);
-
-            while (it->start != it->end && *it->start != '>')
-            {
-                ++it->start;
-            }
-
-            ++it->start;
+            element = new HTMLElement(tok.data);
+            printf("Open tag: %s\n", tok.data);    
+        }
+        else if (tok.type == HT_CloseTag)
+        {
+            parent->AddChildren(element);
+            element = nullptr;
+            printf("Close tag: %s\n", tok.data);
+        }
+        else if (tok.type == HT_Data)
+        {
+            element->SetData(tok.data);
+            printf("Data: %s\n", tok.data);
         }
 
-        pdata = data_buf;
-        trim_buf(it);
-        
-        while (it->start != it->end)
-        {
-            /*
-            if (is_open_comment(it))
-            {
-                // Trim the comment
-
-                while (it->start != it->end)
-                {
-                    if (is_close_comment(it))
-                    {
-                        it->start += 3;
-                        break;
-                    }
-
-                    ++it->start;
-                }
-            }*/
-
-            if (*it->start == '<')
-            {
-                break;
-            }
-
-            if (*it->start == '\n')
-            {
-                *pdata++ = ' ';
-            }
-            else
-            {
-                *pdata++ = *it->start;
-            }
-
-            ++it->start;
-        }
-        *pdata = '\0';
-
-        if (strlen(data_buf) != 0)
-        {
-            HTMLToken tok(HT_Data);
-            strcpy(tok.data, data_buf);
-// TODO avoid copying the data buffer
-            tokens.push_back(tok);
-        }
+        ++start;
     }
+}
+
+static void create_dom(const std::vector<HTMLToken> &tokens)
+{
+    HTMLElement rootElement("html");
+
+    assemble_dom(&rootElement, tokens.begin(), tokens.end());
+
+    rootElement.PrintTree();
 }
 
 static void parse_and_run(const char *path)
@@ -260,12 +135,7 @@ static void parse_and_run(const char *path)
 
     // Ok, we have all the tokens so lets create a dom now
 
-    
-
-    for (HTMLToken &tok : tokens)
-    {
-        printf("Token : %d, data: %s\n", tok.type, tok.data);
-    }
+    create_dom(tokens);
 }
 
 int main(int argc, char **argv)
---
+++ parser.h
@@ -0,0 +1,241 @@
+#pragma once
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <cstring>
+
+// 1 Mb
+#define MAX_FILE_BUFFER (1 * 1024 * 1024)
+
+struct buf_it
+{
+    char *start;
+    char *end;
+};
+
+static char *load_all(const char *path, size_t *out_sz)
+{
+    FILE* fp;
+    char *buf;
+    unsigned long long sz;
+
+    if ((fp = fopen(path, "rb")) == NULL)
+    {
+        return NULL;
+    }
+
+    fseek(fp, 0, SEEK_END);
+    sz = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+
+    if (sz >= MAX_FILE_BUFFER)
+    {
+        fprintf(stderr, "File size exceeds max buffer size\n");
+        fclose(fp);
+        return NULL;
+    }
+
+    buf = (char *)malloc(sz);
+    if (!buf)
+    {
+        fprintf(stderr, "Could not alloc the required memory\n");
+        fclose(fp);
+        return NULL;
+    }
+
+    if (fread(buf, 1, sz, fp) != sz)
+    {
+        fprintf(stderr, "Could not read the file\n");
+        free(buf);
+        fclose(fp);
+        return NULL;
+    }
+
+    *out_sz = sz;
+
+    fclose(fp);
+    return buf;
+}
+
+static bool char_is_any(char ch, const char *any)
+{
+    while (*any)
+    {
+        if (ch == *any)
+        {
+            return true;
+        }
+
+        ++any;
+    }
+
+    return false;
+}
+
+static char *parse_ident(buf_it *it, char* buf)
+{
+    char *pbuf = buf, ch;
+
+    while (it->start != it->end)
+    {
+        ch = *it->start;
+
+        if (isalpha(ch) || (pbuf != buf && (isalnum(ch) || char_is_any(ch, "-"))))
+        {
+            *buf++ = ch;
+            ++it->start;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    if (buf == pbuf)
+    {
+        return NULL;
+    }
+
+    *buf = '\0';
+    return pbuf;
+}
+
+static void trim_buf(buf_it *it)
+{
+    while (it->start != it->end && isspace(*it->start))
+    {
+        ++it->start;
+    }
+}
+
+// Checks if the buffer contains "<!--" at the current position
+static bool is_open_comment(buf_it *it)
+{
+    if (it->end - it->start >= 4)
+    {
+        if (strcmp(it->start, "<!--") == 0)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+static bool is_close_comment(buf_it *it)
+{
+    if (it->end - it->start >= 3)
+    {
+        if (strcmp(it->start, "-->") == 0)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+#define HT_OpenTag 1
+#define HT_CloseTag 2
+#define HT_Data 3
+
+struct HTMLToken
+{
+    int type;
+    char data[128];
+
+    HTMLToken(int type)
+        : type(type)
+    {
+    }
+};
+
+static void parse(buf_it *it, std::vector<HTMLToken> &tokens)
+{
+    char ident_buf[64];
+    bool close_tag;
+    char data_buf[128];
+    char *pdata = data_buf;
+
+    while (it->start != it->end)
+    {
+        if (*it->start == '<')
+        {
+            ++it->start;
+
+            trim_buf(it);
+
+            if (*it->start == '/')
+            {
+                close_tag = true;
+                ++it->start;
+                trim_buf(it);
+            }
+            else
+            {
+                close_tag = false;
+            }
+
+            HTMLToken tok(close_tag ? HT_CloseTag : HT_OpenTag);
+            parse_ident(it, tok.data);
+
+            tokens.push_back(tok);
+
+            while (it->start != it->end && *it->start != '>')
+            {
+                ++it->start;
+            }
+
+            ++it->start;
+        }
+
+        pdata = data_buf;
+        trim_buf(it);
+        
+        while (it->start != it->end)
+        {
+            /*
+            if (is_open_comment(it))
+            {
+                // Trim the comment
+
+                while (it->start != it->end)
+                {
+                    if (is_close_comment(it))
+                    {
+                        it->start += 3;
+                        break;
+                    }
+
+                    ++it->start;
+                }
+            }*/
+
+            if (*it->start == '<')
+            {
+                break;
+            }
+
+            if (*it->start == '\n')
+            {
+                *pdata++ = ' ';
+            }
+            else
+            {
+                *pdata++ = *it->start;
+            }
+
+            ++it->start;
+        }
+        *pdata = '\0';
+
+        if (strlen(data_buf) != 0)
+        {
+            HTMLToken tok(HT_Data);
+            strcpy(tok.data, data_buf);
+// TODO avoid copying the data buffer
+            tokens.push_back(tok);
+        }
+    }
+}
